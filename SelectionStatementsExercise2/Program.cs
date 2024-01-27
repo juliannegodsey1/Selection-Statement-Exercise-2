9033ab4e35833fc6e6f9ef97aaf61c7bc43ee55d
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-01-27 15:44:22 -0500

    redo complete

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I don't like math!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is cool!");
+                    break;
+
+                case "english":
+                    Console.WriteLine("I like english too");
+                    break;
+
+                case "history":
+                    Console.WriteLine("History is boring!");
+                    break;
+
+                default:
+                    Console.WriteLine("I don't know that subject.");
+                    break;
+   
+            }
+
         }
     }
 }
